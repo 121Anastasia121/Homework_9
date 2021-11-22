f623084b568e56078a714b251cbbd3211df8b373
121Anastasia121 <86976844+121Anastasia121@users.noreply.github.com>
2021-11-22 15:39:41 +0300

    Homework_9

---
+++ index.js
@@ -0,0 +1,40 @@
+// const arr = [2,3,4,5];
+// let result=1;
+// for (let index = 0; index < arr.length; index++) {
+//   result*=arr[index];
+//   console.log(result);
+// }
+
+
+// const arr = [1,2,5,9,4,13,4,10];
+// for (let index = 0; index = 4; index++) {
+//   console.log (arr[index]);
+
+//   if (arr[index] === 4) {
+//     console.log('Truth');
+//     break;
+//   }
+// }
+
+
+// const arr = [12,15,20,25,59,79];
+// let result=0;
+// for (let index = 0; index < arr.length; index++) {
+//   result+=arr[index];
+// }
+// console.log(result);
+// result=result / arr.length;
+// console.log(result);
+
+
+// const arr = [1,2,3,4,5,6,7,8,9,10];
+// let result=0;
+// for (let index = 0; index < arr.length; index++) {
+//   result=arr[index]%2===0;
+  
+//   if (arr[index]%2===0) {
+//     console.log(arr[index], 'even');
+//   } else {
+//     console.log(arr[index], 'odd');
+//   }
+// }
